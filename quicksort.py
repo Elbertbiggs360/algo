61e4c603ba889fb7a081eab59f9d42c043ed8b8b
Bruce Bigirwenkya <brucebigirwenkya@gmail.com>
2019-08-15 12:51:45 +0300

    Quicksort

--- quicksort.py
+++ quicksort.py
@@ -1,22 +1,19 @@
-def quick_sort_interface(arr):
-    quick_sort(arr, 0, len(arr) - 1)
+def quickSort(arr):
+    less = []
+    pivotList = []
+    more = []
 
-
-def quick_sort(unsorted_list, start_index, end_index):
-    if start_index < end_index:
-        partition_index = partition(unsorted_list, start_index, end_index)
-        quick_sort(unsorted_list, start_index, partition_index - 1)
-        quick_sort(unsorted_list, partition_index + 1, end_index)
-
-
-def partition(arr, low_index, high_index):
-    divider = low_index
-    pivot = high_index
-
-    for index in range(low_index, high_index):
-        if arr[index] < arr[pivot]:
-            arr[index], arr[divider] = arr[divider], arr[index]
-            divider += 1
-    arr[divider], arr[pivot] = arr[pivot], arr[divider]
-
-    return divider
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[0]
+        for i in arr:
+            if i < pivot:
+                less.append(i)
+            elif i > pivot:
+                more.append(i)
+            else:
+                pivotList.append(i)
+        less = quickSort(less)
+        more = quickSort(more)
+        return less + pivotList + more
