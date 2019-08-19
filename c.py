747484e25a24b0ad98c586b3519abe3b6583c461
Bruce Bigirwenkya <brucebigirwenkya@gmail.com>
2019-08-19 14:51:36 +0300

    Sorting revision using lambda key

--- c.py
+++ c.py
@@ -1,19 +1,11 @@
-fname = input('File name:')
-if len(fname) < 1: fname = 'romeo.txt'
-hand = open(fname)
-
 di = dict()
-for lin in hand:
-    lin = lin.rstrip()
-    wds = lin.split()
-    for w in wds:
-        di[w] = di.get(w, 0) + 1
+with open('clown.txt') as hand:
+    for lin in hand:
+        lin = lin.rstrip()
+        wds = lin.split()
+        for w in wds:
+            di[w] = di.get(w, 0) + 1
 
-sortedList = list()
-for k, v in di.items():
-    reversedItem = (v, k)
-    sortedList.append(reversedItem)
-
-sortedList = sorted(sortedList, reverse=True)
-for val, key in sortedList[:10]:
+sortedList = sorted(di.items(), key=lambda x: x[1], reverse=True)
+for val, key in sortedList:
     print(key, val)
